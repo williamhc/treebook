c3b26db751b592f6df1a3946a23da884c7f53f9d
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2012-12-06 07:24:27 -0600

    Scoping statuses to be actually user centric.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -28,6 +28,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
+
   test "should create status when logged in" do
     sign_in users(:willhc)
 
@@ -36,6 +37,17 @@ class StatusesControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should create a status for the current user when logged in" do
+    sign_in users(:willhc)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:test_user123).id }  
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:willhc).id
   end
 
   test "should show status" do
@@ -61,11 +73,29 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should update status" do
+  test "should update status when logged in" do
     sign_in users(:willhc)
     put :update, id: @status, status: { content: @status.content }
     assert_response :redirect
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in" do
+    sign_in users(:willhc)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:test_user123).id }
+    assert_response :redirect
+    assert_redirected_to status_path(assigns(:status))
+
+    assert_equal assigns(:status).user_id, users(:willhc).id
+  end
+
+  test "should not update the status if nothing has changed" do
+    sign_in users(:willhc)
+    put :update, id: @status
+    assert_response :redirect
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:willhc).id
+
   end
 
   test "should destroy status" do
